693cb6cf8d9a246c9d9ed9a2470fa7fe06377cbb
Aksenova63 <agulkovae@gmail.com>
2025-01-19 00:18:36 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+#Calculator
+a = float(input("Введите первое число"))
+b = float(input("Введите второе число"))
+operation = input("Что сделать?(+,-,*,/):")
+if operation == "+":
+    result = a + b
+elif operation == "-":
+    result = a - b
+elif operation == "*":
+    result = a * b
+elif operation == "/":
+    result = a / b
+print(result)
+input()
+
+
